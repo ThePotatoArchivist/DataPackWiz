0c5c79f34a0cb9cdc8f6fdfc424d9907a3f8e5fb
JosiahFu <josiahfu@gmail.com>
2025-05-03 13:22:23 -0700

    WIP x2

--- src/main/kotlin/archives/tater/datapackwiz/lib/argument/TagArgumentType.kt
+++ src/main/kotlin/archives/tater/datapackwiz/lib/argument/TagArgumentType.kt
@@ -1,25 +1,24 @@
 package archives.tater.datapackwiz.lib.argument
 
+import archives.tater.datapackwiz.lib.set
+import com.google.gson.JsonObject
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.exceptions.CommandSyntaxException
 import com.mojang.brigadier.suggestion.Suggestions
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
-import net.minecraft.block.Block
 import net.minecraft.command.CommandRegistryAccess
 import net.minecraft.command.CommandSource
-import net.minecraft.entity.EntityType
-import net.minecraft.fluid.Fluid
-import net.minecraft.item.Item
+import net.minecraft.command.argument.serialize.ArgumentSerializer
+import net.minecraft.command.argument.serialize.ArgumentSerializer.ArgumentTypeProperties
+import net.minecraft.network.PacketByteBuf
 import net.minecraft.registry.*
 import net.minecraft.registry.tag.TagKey
 import net.minecraft.util.Identifier
-import net.minecraft.world.event.GameEvent
 import java.util.concurrent.CompletableFuture
-import kotlin.reflect.KClass
 
-abstract class TagArgumentType<T>(private val registry: RegistryKey<Registry<T>>, registryAccess: CommandRegistryAccess) : ArgumentType<TagKey<*>> {
+class TagArgumentType(private val registry: RegistryKey<Registry<Any>>, registryAccess: CommandRegistryAccess) : ArgumentType<TagKey<*>> {
     private val registryWrapper = registryAccess.createWrapper(registry)
 
     @Throws(CommandSyntaxException::class)
@@ -34,30 +33,37 @@ abstract class TagArgumentType<T>(private val registry: RegistryKey<Registry<T>>
 
 //    override fun getExamples(): Collection<String> = EXAMPLES
 
-    abstract class Factory<T: Any>(private val registry: RegistryKey<Registry<T>>, val `class`: KClass<out TagArgumentType<T>>, val factory: (CommandRegistryAccess) -> TagArgumentType<T>) {
-        @Suppress("UNCHECKED_CAST")
-        fun get(context: CommandContext<*>, name: String): TagKey<T> =
-            context.getArgument(name, TagKey::class.java) as TagKey<T>
-    }
+    class Serializer : ArgumentSerializer<TagArgumentType, Serializer.Properties> {
+        override fun writePacket(properties: Properties, buf: PacketByteBuf) {
+            buf.writeRegistryKey(properties.registry);
+        }
 
-    class ITEM(registryAccess: CommandRegistryAccess) : TagArgumentType<Item>(RegistryKeys.ITEM, registryAccess) {
-        companion object : Factory<Item>(RegistryKeys.ITEM, ITEM::class, ::ITEM)
-    }
+        override fun fromPacket(buf: PacketByteBuf): Properties =
+            Properties(buf.readRegistryKey(ROOT_KEY))
 
-    class BLOCK(registryAccess: CommandRegistryAccess) : TagArgumentType<Block>(RegistryKeys.BLOCK, registryAccess) {
-        companion object : Factory<Block>(RegistryKeys.BLOCK, BLOCK::class, ::BLOCK)
-    }
+        override fun getArgumentTypeProperties(argumentType: TagArgumentType): Properties =
+            Properties(argumentType.registry)
 
-    class FLUID(registryAccess: CommandRegistryAccess) : TagArgumentType<Fluid>(RegistryKeys.FLUID, registryAccess) {
-        companion object : Factory<Fluid>(RegistryKeys.FLUID, FLUID::class, ::FLUID)
-    }
+        override fun writeJson(properties: Properties, json: JsonObject) {
+            json["registry"] = properties.registry.value.toString()
+        }
 
-    class ENTITY_TYPE(registryAccess: CommandRegistryAccess) : TagArgumentType<EntityType<*>>(RegistryKeys.ENTITY_TYPE, registryAccess) {
-        companion object : Factory<EntityType<*>>(RegistryKeys.ENTITY_TYPE, ENTITY_TYPE::class, ::ENTITY_TYPE)
-    }
+        companion object {
+            val ROOT_KEY: RegistryKey<Registry<Registry<Any>>> = RegistryKey.ofRegistry(Identifier("root"))
+        }
 
-    class GAME_EVENT(registryAccess: CommandRegistryAccess) : TagArgumentType<GameEvent>(RegistryKeys.GAME_EVENT, registryAccess) {
-        companion object : Factory<GameEvent>(RegistryKeys.GAME_EVENT, GAME_EVENT::class, ::GAME_EVENT)
+        inner class Properties(
+            val registry: RegistryKey<Registry<Any>> // wip
+        ) : ArgumentTypeProperties<TagArgumentType> {
+            override fun createType(commandRegistryAccess: CommandRegistryAccess): TagArgumentType =
+                TagArgumentType(registry, commandRegistryAccess)
+
+            override fun getSerializer(): ArgumentSerializer<TagArgumentType, Properties> {
+                return this@Serializer
+            }
+
+        }
+
     }
 
     companion object {
